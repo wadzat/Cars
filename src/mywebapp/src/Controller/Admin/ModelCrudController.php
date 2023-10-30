36ba5c8320e1b147ab0ecc6688c513f51b1dbce4
Nicolas Perrot <wadzat@gmail.com>
2023-10-30 14:59:17 +0100

    configure fields display, searching and sorting in easyAdmin

--- src/mywebapp/src/Controller/Admin/ModelCrudController.php
+++ src/mywebapp/src/Controller/Admin/ModelCrudController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Model;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class ModelCrudController extends AbstractCrudController
 {
@@ -15,13 +19,29 @@ class ModelCrudController extends AbstractCrudController
         return Model::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Modèle')
+            ->setEntityLabelInPlural('Modèles')
+            ->setDefaultSort(['name' => 'ASC']);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
+            IdField::new('id')->hideOnForm(),
             TextField::new('name'),
             AssociationField::new('brand'),
-            AssociationField::new('energy'),
+            AssociationField::new('energy')->hideOnIndex(),
             IntegerField::new('power')
         ];
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('brand'))
+            ->add(EntityFilter::new('energy'));
     }
 }
